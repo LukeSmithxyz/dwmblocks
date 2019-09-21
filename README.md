4f6b1e11f248ed1a5d524b63d7a55c7c7ccb8d32
Luke Smith <luke@lukesmith.xyz>
2019-09-21 15:37:13 -0400

    dox and stuff rmd

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Modular status bar for dwm written in c.
 # modifying blocks
 The statusbar is made from text output from commandline programs.
 Blocks are added and removed by editing the blocks.h header file.
+# signalling changes
+For example, the audio module has the update signal 10 by default.
+Thus, running `pkill -RTMIN+10 dwmblocks` will update it.
--- blocks.h
+++ blocks.h
@@ -10,17 +10,3 @@ static const Block blocks[] = {
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim = ' ';
-
-	/* {"🔊 ", "x=\"$(amixer get Master)\"; echo \"$x\" | grep -o \"\\[off\\]\" || echo \"$x\" | grep -o [0-9]*%", 0, 10}, */
-	/* {"", "sed /📦0/d ~/.pacupdate",					0,		9}, */
-
-	/* {"🧠 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0}, */
-
-	/* {"💻 ", "sed 's/000$/°C/' /sys/class/thermal/thermal_zone0/temp", 30, 21}, */
-
-	//{"🎼 ", "mpc status | sed \"/^volume:/d;/\\[playing\\]/d;s/^\\[paused\\].*/⏸/\"", 0, 11},
-
-	//{"☀ ", "xbacklight | sed 's/\\..*//'",					0,		11},
-
-	/* {"🌡 ", "sensors | awk '/^temp1:/{print $2}'",				5,		0}, */
-
