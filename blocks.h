a6c82c2148384296df1be6069f885c63e77ef111
Luke Smith <luke@lukesmith.xyz>
2020-02-01 12:43:28 -0500

    simplification for emojis

--- blocks.h
+++ blocks.h
@@ -1,12 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat /tmp/recordingicon",	0,	9},
-	{"📬 ", "find ~/.local/share/mail/*/INBOX/new -type f | wc -l", 0, 13},
-	{"🔊 ", "amixer get Master | grep -o \"\\(\\[off\\]\\|[0-9]*%\\)\"", 0, 10},
-	{"🔋 ", "sed \"s/$/%/\" /sys/class/power_supply/BAT?/capacity", 5, 12},
-	{"🕗 ", "date '+%Y %b %d (%a) %I:%M%p'",	60,	0},
+	/* {"", "cat /tmp/recordingicon",	0,	9}, */
+	/* {"", "mpc status | grep -v ^volume | head -n 1", 0, 11}, */
+	{"mail:", "find ~/.local/share/mail/*/INBOX/new -type f | wc -l", 0, 12},
+	{"vol:", "amixer get Master | tail -n1 | sed -r 's/.*\\[(.*)%\\].*/\\1/'", 0, 10},
+	{"bat:", "sed \"s/$/%/\" /sys/class/power_supply/BAT?/capacity", 5, 0},
+	{"", "date '+%b %d (%a) %I:%M%p'",	60,	0},
+	/* weather: 5 */
+	/* news: 6 */
+	/* torrent: 7 */
+	/* packages: 8 */
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = ' ';
+static char delim = '|';
