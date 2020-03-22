781eacae90b610054569251e707e549464b05b7f
Luke Smith <luke@lukesmith.xyz>
2020-03-22 09:09:11 -0400

    record error suppressed

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat /tmp/recordingicon",	0,	9},
+	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"",	"music",	0,	11}, */
 	{"",	"pacpackages",	0,	8},
 	/* {"",	"crypto",	0,	13}, */
