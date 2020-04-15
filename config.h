c76a6c7c1db05c931387ed53d6403e002c734f7d
Luke Smith <luke@lukesmith.xyz>
2020-04-15 14:42:20 -0400

    help renamed to help-icon

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const Block blocks[] = {
 	/* {"music",	0,	11}, */
 	{"pacpackages",	0,	8},
 	/* {"crypto",	0,	13}, */
-	{"torrent",	20,	7},
-	{"news",		0,	6},
+	/* {"torrent",	20,	7}, */
+	{"news",	0,	6},
 	/* {"memory",	10,	14}, */
 	/* {"cpu",		10,	13}, */
 	/* {"moonphase",	18000,	5}, */
@@ -16,8 +16,13 @@ static const Block blocks[] = {
 	{"battery",	5,	3},
 	{"clock",	60,	1},
 	{"internet",	5,	4},
-	{"help",		0,	15},
+	{"help-icon",	0,	15},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim = '|';
+
+// Have dwmblocks automatically recompile and run when you edit this file in
+// vim with the following line in your vimrc/init.vim:
+
+// autocmd BufWritePost */dwmblocks/config.h !sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
