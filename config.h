a0ebcc3f5448204a9414691fea0f496404f81560
Luke Smith <luke@lukesmith.xyz>
2020-05-04 15:26:24 -0400

    nettraf opt added, vol not manually updated, news moved

--- config.h
+++ config.h
@@ -4,15 +4,16 @@ static const Block blocks[] = {
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	{"",	"music",	0,	11},
 	{"",	"pacpackages",	0,	8},
+	{"",	"news",		0,	6},
 	/* {"",	"crypto",	0,	13}, */
 	{"",	"torrent",	20,	7},
-	{"",	"news",		0,	6},
 	/* {"",	"memory",	10,	14}, */
 	/* {"",	"cpu",		10,	13}, */
 	/* {"",	"moonphase",	18000,	5}, */
 	{"",	"weather",	18000,	5},
 	{"",	"mailbox",	180,	12},
-	{"",	"volume",	1,	10},
+	/* {"",	"nettraf",	1,	16}, */
+	{"",	"volume",	0,	10},
 	{"",	"battery | tr \'\n\' \' \'",	5,	3},
 	{"",	"clock",	60,	1},
 	{"",	"internet",	5,	4},
