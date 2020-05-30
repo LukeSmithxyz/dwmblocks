e2ca6028d61859b66816fadeb6103fee1fa58314
Zaky Bilfagih <55378008+mackarelfish@users.noreply.github.com>
2020-05-30 12:44:06 +0700

    Commented out music from default config.h

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	{"",	"music",	0,	11},
+	/* {"",	"music",	0,	11},*/
 	{"",	"pacpackages",	0,	8},
 	{"",	"news",		0,	6},
 	/* {"",	"crypto",	0,	13}, */
