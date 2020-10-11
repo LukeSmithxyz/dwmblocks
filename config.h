791a837fac7089cd70d9c950e93227b71a134678
Luke Smith <luke@lukesmith.xyz>
2020-10-11 15:18:55 -0400

    weather -> forceast; different defaults

--- config.h
+++ config.h
@@ -1,21 +1,25 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	/* {"⌨", "kbselect", 0, 30}, */
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	/* {"",	"music",	0,	11},*/
+	/* {"",	"music",	0,	11}, */
 	{"",	"pacpackages",	0,	8},
 	{"",	"news",		0,	6},
 	/* {"",	"crypto",	0,	13}, */
-	/* {"",	"price bat \"Basic Attention Token\" 🦁",	0,	20}, */
-	/* {"",	"price btc Bitcoin 💰",				0,	21}, */
-	/* {"",	"price lbc \"LBRY Token\" 📚",			0,	22}, */
-	{"",	"torrent",	20,	7},
+	/* {"",	"price lbc \"LBRY Token\" 📚",			9000,	22}, */
+	/* {"",	"price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
+	/* {"",	"price link \"Chainlink\" 🔗",			300,	25}, */
+	/* {"",	"price xmr \"Monero\" 🔒",			9000,	24}, */
+	/* {"",	"price eth Ethereum 🍸",	9000,	23}, */
+	/* {"",	"price btc Bitcoin 💰",				9000,	21}, */
+	/* {"",	"torrent",	20,	7}, */
 	/* {"",	"memory",	10,	14}, */
 	/* {"",	"cpu",		10,	18}, */
 	/* {"",	"moonphase",	18000,	17}, */
-	{"",	"weather",	18000,	5},
+	{"",	"forecast",	18000,	5},
 	{"",	"mailbox",	180,	12},
-	/* {"",	"nettraf",	1,	16}, */
+	{"",	"nettraf",	1,	16},
 	{"",	"volume",	0,	10},
 	{"",	"battery",	5,	3},
 	{"",	"clock",	60,	1},
