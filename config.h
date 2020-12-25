fd15bc9b95da2e352649b02e5b36cf2975ddf1fe
Luke Smith <luke@lukesmith.xyz>
2020-12-25 08:03:23 -0500

    scripts prefixed with sb- to avoid conflicts rename scripts or update voidrice repo

--- config.h
+++ config.h
@@ -3,28 +3,29 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/* {"⌨", "kbselect", 0, 30}, */
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	/* {"",	"music",	0,	11}, */
-	{"",	"pacpackages",	0,	8},
-	{"",	"news",		0,	6},
-	/* {"",	"crypto",	0,	13}, */
-	/* {"",	"price lbc \"LBRY Token\" 📚",			9000,	22}, */
-	/* {"",	"price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
-	/* {"",	"price link \"Chainlink\" 🔗",			300,	25}, */
-	/* {"",	"price xmr \"Monero\" 🔒",			9000,	24}, */
-	/* {"",	"price eth Ethereum 🍸",	9000,	23}, */
-	/* {"",	"price btc Bitcoin 💰",				9000,	21}, */
-	/* {"",	"torrent",	20,	7}, */
-	/* {"",	"memory",	10,	14}, */
-	/* {"",	"cpu",		10,	18}, */
-	/* {"",	"moonphase",	18000,	17}, */
-	{"",	"forecast",	18000,	5},
-	{"",	"mailbox",	180,	12},
-	{"",	"nettraf",	1,	16},
-	{"",	"volume",	0,	10},
-	{"",	"battery",	5,	3},
-	{"",	"clock",	60,	1},
-	{"",	"internet",	5,	4},
-	{"",	"help-icon",	0,	15},
+	{"",	"sb-tasks",	10,	26},
+	{"",	"sb-music",	0,	11},
+	{"",	"sb-pacpackages",	0,	8},
+	{"",	"sb-news",		0,	6},
+	/* {"",	"sb-crypto",	0,	13}, */
+	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
+	/* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
+	/* {"",	"sb-price link \"Chainlink\" 🔗",			300,	25}, */
+	/* {"",	"sb-price xmr \"Monero\" 🔒",			9000,	24}, */
+	/* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
+	/* {"",	"sb-price btc Bitcoin 💰",				9000,	21}, */
+	{"",	"sb-torrent",	20,	7},
+	/* {"",	"sb-memory",	10,	14}, */
+	/* {"",	"sb-cpu",		10,	18}, */
+	/* {"",	"sb-moonphase",	18000,	17}, */
+	{"",	"sb-forecast",	18000,	5},
+	{"",	"sb-mailbox",	180,	12},
+	{"",	"sb-nettraf",	1,	16},
+	{"",	"sb-volume",	0,	10},
+	{"",	"sb-battery",	5,	3},
+	{"",	"sb-clock",	60,	1},
+	{"",	"sb-internet",	5,	4},
+	{"",	"sb-help-icon",	0,	15},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
