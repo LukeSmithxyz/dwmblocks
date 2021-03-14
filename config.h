d68479db242c5f55572af169c1ba3d5bf2f38555
dfuehrer <dfuehrer@email.arizona.edu>
2021-03-14 04:52:41 -0700

    copied over the master config since it is unrelated to the fix

--- config.h
+++ config.h
@@ -1,23 +1,34 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	/* {"",	"music",	0,	11},*/
-	/* {"",	"pacpackages",	0,	8}, */
-	/* {"",	"memory",	10,	14}, */
-	/* {"",	"weather",	18000,	5}, */
-	/* {"",	"mailbox",	180,	12}, */
-	/* {"",	"nettraf",	1,	16}, */
-	/* {"",	"volume",	0,	10}, */
-    /* {"",	"internet",	5,	4}, */
-    {"",    "stalonetray.sh", 0, 27},
-    /* {"?",   "sleep 2s && date", 1, 8}, */
-    {"",	"cpupercent.sh",	1,	19},
-	{"",	"battery",	5,	3},
-	{"",	"clock",	1,	1},
+	/* {"⌨", "sb-kbselect", 0, 30}, */
+	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
+	{"",	"sb-tasks",	10,	26},
+	{"",	"sb-music",	0,	11},
+	{"",	"sb-pacpackages",	0,	8},
+	{"",	"sb-news",		0,	6},
+	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
+	/* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
+	/* {"",	"sb-price link \"Chainlink\" 🔗",			300,	25}, */
+	/* {"",	"sb-price xmr \"Monero\" 🔒",			9000,	24}, */
+	/* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
+	/* {"",	"sb-price btc Bitcoin 💰",				9000,	21}, */
+	{"",	"sb-torrent",	20,	7},
+	/* {"",	"sb-memory",	10,	14}, */
+	/* {"",	"sb-cpu",		10,	18}, */
+	/* {"",	"sb-moonphase",	18000,	17}, */
+	{"",	"sb-forecast",	18000,	5},
+	{"",	"sb-mailbox",	180,	12},
+	{"",	"sb-nettraf",	1,	16},
+	{"",	"sb-volume",	0,	10},
+	{"",	"sb-battery",	5,	3},
+	{"",	"sb-clock",	60,	1},
+	{"",	"sb-internet",	5,	4},
+	{"",	"sb-help-icon",	0,	15},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = "|";
+static char *delim = " ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
