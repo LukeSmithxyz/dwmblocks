4082ffc3c7dbdbc34ef05c8b24cfa443715d3124
eifyn <92360492+eifyn@users.noreply.github.com>
2021-12-31 03:30:52 +0000

    Update config.h

--- config.h
+++ config.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	/* {"⌨", "sb-kbselect", 0, 30}, */
+	{"⌨", "sb-kbselect", 0, 30},
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	{"",	"sb-tasks",	10,	26},
 	{"",	"sb-music",	0,	11},
-	{"",	"sb-pacpackages",	0,	8},
+	{"",	"sb-pacpackages",	18000,	8},
 	{"",	"sb-news",		0,	6},
 	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
 	/* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
@@ -16,15 +16,15 @@ static const Block blocks[] = {
 	{"",	"sb-torrent",	20,	7},
 	/* {"",	"sb-memory",	10,	14}, */
 	/* {"",	"sb-cpu",		10,	18}, */
-	/* {"",	"sb-moonphase",	18000,	17}, */
+	{"",	"sb-moonphase",	18000,	17},
 	{"",	"sb-forecast",	18000,	5},
 	{"",	"sb-mailbox",	180,	12},
 	{"",	"sb-nettraf",	1,	16},
 	{"",	"sb-volume",	0,	10},
-	{"",	"sb-battery",	5,	3},
+	/* {"",	"sb-battery",	5,	3}, */
 	{"",	"sb-clock",	60,	1},
 	{"",	"sb-internet",	5,	4},
-	{"",	"sb-help-icon",	0,	15},
+	/* {"",	"sb-help-icon",	0,	15}, */
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
