6ae770c5cd10e64ad42683fdccefe58823dd8f0c
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2023-04-04 07:30:25 +0000

    Give status bar codes as parameters for async dwmblocks modules

--- config.h
+++ config.h
@@ -7,12 +7,12 @@ static const Block blocks[] = {
 	{"",	"sb-music",	0,	11},
 	{"",	"sb-pacpackages",	0,	8},
 	{"",	"sb-news",		0,	6},
-	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
-	/* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
-	/* {"",	"sb-price link \"Chainlink\" 🔗",			300,	25}, */
-	/* {"",	"sb-price xmr \"Monero\" 🔒",			9000,	24}, */
-	/* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
-	/* {"",	"sb-price btc Bitcoin 💰",				9000,	21}, */
+	/* {"",	"sb-price 22 lbc \"LBRY Token\" 📚",			9000,	22}, */
+	/* {"",	"sb-price 20 bat \"Basic Attention Token\" 🦁",	9000,	20}, */
+	/* {"",	"sb-price 25 link Chainlink 🔗",			300,	25}, */
+	/* {"",	"sb-price 24 xmr Monero 🔒",			9000,	24}, */
+	/* {"",	"sb-price 23 eth Ethereum 🍸",	9000,	23}, */
+	/* {"",	"sb-price 21 btc Bitcoin 💰",				9000,	21}, */
 	{"",	"sb-torrent",	20,	7},
 	/* {"",	"sb-memory",	10,	14}, */
 	/* {"",	"sb-cpu",		10,	18}, */
@@ -25,6 +25,7 @@ static const Block blocks[] = {
 	{"",	"sb-battery",	5,	3},
 	{"",	"sb-clock",	60,	1},
 	{"",	"sb-internet",	5,	4},
+	/* {"",	"sb-iplocate", 0,	27}, */
 	{"",	"sb-help-icon",	0,	15},
 };
 
