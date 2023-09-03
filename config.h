e17cab69782cc55615d368811ec89a64415c34a4
suiso67 <suiso67@macadamia.rocks>
2023-09-03 23:03:16 +0900

    Move auto compilation instruction to README.md

--- config.h
+++ config.h
@@ -29,7 +29,3 @@ static const Block blocks[] = {
 // Sets delimiter between status commands.
 // NULL character ('\0') means no delimiter.
 static char *delim = " ";
-
-// Have dwmblocks automatically recompile and run when you edit this file in
-// vim with the following line in your vimrc/init.vim:
-// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
